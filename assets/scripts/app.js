29366385836139d1b9cf3e9fec3296e9e00ae509
jgalla <jessejgallagher@gmail.com>
2019-01-07 11:15:20 -0500

    Console.log winner of game or draw
    Must add code to prevent clicking occupied cell and to stop gameplay
    when over === true.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,9 +7,13 @@
 // require('./example')
 
 $(() => {
-  const cells = ['','','','','','','','','']
-
+  const cells = ['', '', '', '', '', '', '', '', '']
+  let over = false
   let user = 'X'
+
+  const compare = function (a, b, c) {
+    return a === b && b === c && a !== ''
+  }
 
   for (let i = 0; i < cells.length; i++) {
     $('#cell-' + i).on('click', () => {
@@ -20,9 +24,36 @@ $(() => {
       } else {
         user = 'X'
       }
-
-      console.log(user)
       console.log(cells)
+
+      if (compare(cells[0], cells[1], cells[2])) {
+        console.log(cells[0], 'won the game')
+        over = true
+      } else if (compare(cells[3], cells[4], cells[5])) {
+        console.log(cells[3], 'won the game')
+        over = true
+      } else if (compare(cells[6], cells[7], cells[8])) {
+        console.log(cells[6], 'won the game')
+        over = true
+      } else if (compare(cells[0], cells[3], cells[6])) {
+        console.log(cells[0], 'won the game')
+        over = true
+      } else if (compare(cells[1], cells[4], cells[7])) {
+        console.log(cells[1], 'won the game')
+        over = true
+      } else if (compare(cells[2], cells[5], cells[8])) {
+        console.log(cells[2], 'won the game')
+        over = true
+      } else if (compare(cells[0], cells[4], cells[8])) {
+        console.log(cells[0], 'won the game')
+        over = true
+      } else if (compare(cells[2], cells[4], cells[6])) {
+        console.log(cells[2], 'won the game')
+        over = true
+      } else if (cells.every(x => x !== '')) {
+        console.log('draw')
+        over = true
+      }
     })
   }
 })
